e69397dfc07a2c83bfd8ae89db46ee38d4f11558
nobytes777 <106572728+nobytes777@users.noreply.github.com>
2022-09-04 09:13:05 -0600

    Alert

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import {
-    View, 
-    FlatList, 
-    Text, 
-    TouchableOpacity, 
-    StyleSheet 
+    View,
+    FlatList,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    Alert
 } from 'react-native';
 import { Avatar, ListItem } from "react-native-elements";
 import Loading from '../components/LoadingComponent';
@@ -24,16 +25,30 @@ const FavoritesScreen = ({ navigation }) => {
         return (
             <SwipeRow rightOpenValue={-100}>
                 <View style={styles.deleteView}>
-                    <TouchableOpacity 
-                        style={styles.deleteTouchable} 
-                        onPress={() => dispatch(toggleFavorite(campsite.id))}
+                    <TouchableOpacity
+                        style={styles.deleteTouchable}
+                        onPress={() => Alert.alert('Delete Favorite?',
+                            'Are you sure you wish to delete the favorite campsite' + campsite.name + '?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log(campsite.name + 'Not Deleted'),
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'OK',
+                                    onPress: () => dispatch(toggleFavorite(campsite.id))
+                                }
+                            ],
+                            { cancelable: false }
+                        )}
                     >
                         <Text style={styles.deleteText}>Delete</Text>
                     </TouchableOpacity>
                 </View>
                 <View>
-                    <ListItem 
-                        onPress={() => 
+                    <ListItem
+                        onPress={() =>
                             navigation.navigate('Directory', {
                                 screen: 'CampsiteInfo',
                                 params: { campsite }
@@ -51,47 +66,47 @@ const FavoritesScreen = ({ navigation }) => {
         );
     };
 
-    if(isLoading) {
+    if (isLoading) {
         return <Loading />;
-    } 
-    if(errMess) {
-        return(
-        <View>
-            <Text>{errMess}</Text>
-        </View>
+    }
+    if (errMess) {
+        return (
+            <View>
+                <Text>{errMess}</Text>
+            </View>
         );
     }
     return (
         <FlatList
-            data={campsitesArray.filter((campsite) => 
+            data={campsitesArray.filter((campsite) =>
                 favorites.includes(campsite.id)
             )}
             renderItem={renderFavoriteItem}
             keyExtractor={(item) => item.id.toString()}
-            />
+        />
 
     )
 };
 
 const styles = StyleSheet.create({
-        deleteView: {
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            flex: 1
-        },
-        deleteTouchable: {
-            backgroundColor: 'red',
-            height: '100%',
-            justifyContent: 'center'
-        },
-        deleteText: {
-            color: 'white',
-            fontWeight: '700',
-            textAlign: 'center',
-            fontSize: 16,
-            width: 100
-        }
-    });
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+});
 
 export default FavoritesScreen;
